3f6e183fc0ee95dbb0b569c9597aa617a2b21bc2
Sunil Gowda S <64064177+sunil3gs98@users.noreply.github.com>
2023-10-26 17:32:47 +0530

    python_application.py

--- python_application.py
+++ python_application.py
@@ -1,11 +1,2 @@
-from flask import Flask
-
-app = Flask(__name__)
-
-@app.route('/')
-def hello():
-    return 'Hello, world!!!!'
-
-if __name__ == '__main__':
-    app.run()
+print("Hello world !!!")
     
